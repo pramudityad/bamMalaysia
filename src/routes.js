9cd6df5af76dadeaa70e0ca9b3bb0e7c5f14905f
Damar Pramuditya <damar.pramuditya@ericsson.com>
2020-07-02 12:56:43 +0700

    lmr child

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const Dashboard = React.lazy(() => import('./views/Dashboard'));
 
 
 const LMRCreation = React.lazy(() => import('./views/MYAssignment/MYASGCreation'));
-const LMRDetailDummy = React.lazy(() => import('./views/MYAssignment/MYASGDetail'));
+const LMRDetail = React.lazy(() => import('./views/MYAssignment/MYASGDetail'));
 const LMRDetailGR = React.lazy(() => import('./views/MYAssignment/MYASGDetailGR'));
 const LMRList = React.lazy(() => import('./views/MYAssignment/MYASGList'));
 
@@ -16,7 +16,7 @@ const routes = [
 
   { path: '/lmr-list', exact: true, name: 'Assignment LMR List', component: LMRList },
   { path: '/lmr-creation', exact: true, name: 'Assignment LMR Creation', component: LMRCreation },
-  { path: '/lmr-detail/:id', exact: true, name: 'Assignment LMR Detail', component: LMRDetailDummy },
+  { path: '/lmr-detail/:id', exact: true, name: 'Assignment LMR Detail', component: LMRDetail },
   { path: '/lmr-detail/:id/gr-detail/:grid', exact: true, name: 'Assignment LMR Detail GR', component: LMRDetailGR },
 ];
 
