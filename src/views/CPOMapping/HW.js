055b458884fca0874056e5c6ce95c6583b72549a
mac <mac@macdeMacBook-Pro.local>
2020-10-22 16:29:02 +0700

    #

--- src/views/CPOMapping/HW.js
+++ src/views/CPOMapping/HW.js
@@ -6,7 +6,7 @@ import {
   postDatatoAPINODE,
   patchDatatoAPINODE,
   deleteDataFromAPINODE2,
-  getDatafromAPINODE
+  getDatafromAPINODE,
 } from "../../helper/asyncFunction";
 import ModalCreateNew from "../Component/ModalCreateNew";
 import Pagination from "react-js-pagination";
@@ -15,11 +15,78 @@ import { saveAs } from "file-saver";
 const DefaultNotif = React.lazy(() =>
   import("../../views/DefaultView/DefaultNotif")
 );
-const modul_name = "HW Mapping"
+const modul_name = "HW Mapping";
 const header = [
-    'LOOKUP REFERENCE',	'REGION',	'REFERENCE LOC ID',	'NEW LOC ID',	'SITE NAME',	'NEW SITE NAME',	'CONFIG',	'PO#',	'LINE',	'DESCRIPTION',	'QTY',	'NW#',	'CNI DATE',	'MAPPING DATE',	'REMARKS',	'PREMR NO.',	'PROCEED BILLING 100%',	'CELCOM USER',	'PCODE',	'UNIT PRICE',	'TOTAL PRICE',	'DISCOUNTED UNIT PRICE',	'DISCOUNTED PO PRICE',	'SO LINE ITEM DESCRIPTION',	'sitePCode',	'VlookupWBS',	'SO NO.',	'WBS  NO.',	'FOR CHECKING PURPOSE ONLY-RASHIDAH',	'HW COA RECEIVED DATE',	'80% BILLING UPON HW COA',	
-    '80% INVOICING NO.','80% INVOICING DATE',	'NI COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON NI',	'20% INVOICING NO.',	'20% INVOICING DATE',	'SSO COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON SSO',	'20% INVOICING NO.',	'20% INVOICING DATE',	'GR NUMBER',	'HW COA RECEIVED DATE',	'40% BILLING UPON COA HW','40% INVOICING NO.','40% INVOICING DATE','Cancelled',	'NI COA date (vlookup from SERVICES MAPPING)',	'40% BILLING UPON COA NI','40% INVOICING  Number','40% INVOICING DATE','Cancelled',	'SSO COA date (vlookup from SERVICES MAPPING)',	'20% BILLING UPON COA SSO',	'20% INVOICING NO.','20% INVOICING DATE','Cancelled',	'Vlookup: SSO 100% in Service (20% in HW)',	'100%  HW COA ',	'100% BILLING UPON HW COA',	'100% INVOICING NO.',
-    '100% INVOICING DATE','REFERENCE LOC ID',	'PO#',	'REFF',	'SITE LIST',	'REFF 2',	'NI',	'SSO',	'REF NI',
+  "LOOKUP REFERENCE",
+  "REGION",
+  "REFERENCE LOC ID",
+  "NEW LOC ID",
+  "SITE NAME",
+  "NEW SITE NAME",
+  "CONFIG",
+  "PO#",
+  "LINE",
+  "DESCRIPTION",
+  "QTY",
+  "NW#",
+  "CNI DATE",
+  "MAPPING DATE",
+  "REMARKS",
+  "PREMR NO.",
+  "PROCEED BILLING 100%",
+  "CELCOM USER",
+  "PCODE",
+  "UNIT PRICE",
+  "TOTAL PRICE",
+  "DISCOUNTED UNIT PRICE",
+  "DISCOUNTED PO PRICE",
+  "SO LINE ITEM DESCRIPTION",
+  "sitePCode",
+  "VlookupWBS",
+  "SO NO.",
+  "WBS  NO.",
+  "FOR CHECKING PURPOSE ONLY-RASHIDAH",
+  "HW COA RECEIVED DATE",
+  "80% BILLING UPON HW COA",
+  "80% INVOICING NO.",
+  "80% INVOICING DATE",
+  "NI COA DATE (VLOOKUP FROM SERVICES MAPPING)",
+  "20% BILLING UPON NI",
+  "20% INVOICING NO.",
+  "20% INVOICING DATE",
+  "SSO COA DATE (VLOOKUP FROM SERVICES MAPPING)",
+  "20% BILLING UPON SSO",
+  "20% INVOICING NO.",
+  "20% INVOICING DATE",
+  "GR NUMBER",
+  "HW COA RECEIVED DATE",
+  "40% BILLING UPON COA HW",
+  "40% INVOICING NO.",
+  "40% INVOICING DATE",
+  "Cancelled",
+  "NI COA date (vlookup from SERVICES MAPPING)",
+  "40% BILLING UPON COA NI",
+  "40% INVOICING  Number",
+  "40% INVOICING DATE",
+  "Cancelled",
+  "SSO COA date (vlookup from SERVICES MAPPING)",
+  "20% BILLING UPON COA SSO",
+  "20% INVOICING NO.",
+  "20% INVOICING DATE",
+  "Cancelled",
+  "Vlookup: SSO 100% in Service (20% in HW)",
+  "100%  HW COA ",
+  "100% BILLING UPON HW COA",
+  "100% INVOICING NO.",
+  "100% INVOICING DATE",
+  "REFERENCE LOC ID",
+  "PO#",
+  "REFF",
+  "SITE LIST",
+  "REFF 2",
+  "NI",
+  "SSO",
+  "REF NI",
 ];
 
 class MappingHW extends React.Component {
@@ -29,44 +96,43 @@ class MappingHW extends React.Component {
       tokenUser: this.props.dataLogin.token,
       roleUser: this.props.dataLogin.role,
       dropdownOpen: new Array(3).fill(false),
-    all_data: []
+      all_data: [],
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {}
 
-  }
-
-  exportTemplate = () => {
+  exportTemplate = async () => {
     const wb = new Excel.Workbook();
     const ws = wb.addWorksheet();
 
     ws.addRow(header);
     for (let i = 1; i < header.length + 1; i++) {
-      ws.getCell(numToSSColumn(i) + '1').fill = { type: 'pattern',
-      pattern:'solid',
-      fgColor:{argb:'FFFFFF00'},
-      bgColor:{argb:'A9A9A9'}};
+      ws.getCell(numToSSColumn(i) + "1").fill = {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "FFFFFF00" },
+        bgColor: { argb: "A9A9A9" },
+      };
     }
     const PPFormat = await wb.xlsx.writeBuffer();
     saveAs(new Blob([PPFormat]), modul_name + " Template.xlsx");
+  };
 
-  }
-
-  render(){
-      return(
-          <div className="animated fadeIn">
-              <DefaultNotif
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <DefaultNotif
           actionMessage={this.state.action_message}
           actionStatus={this.state.action_status}
         />
-       <Row>
+        <Row>
           <Col xl="12">
             <Card style={{}}>
               <CardHeader>
                 <span style={{ marginTop: "8px", position: "absolute" }}>
                   {" "}
-                  List{" "}{modul_name}
+                  List {modul_name}
                 </span>
                 <div
                   className="card-header-actions"
@@ -74,14 +140,18 @@ class MappingHW extends React.Component {
                 >
                   <div>
                     <div>
-                        <Button block color="success" size="sm" onClick={this.togglecreateModal}>
-                          <i className="fa fa-plus-square" aria-hidden="true">
-                            {" "}
-                            &nbsp;{" "}
-                          </i>{" "}
-                          New
-                        </Button>
-       
+                      <Button
+                        block
+                        color="success"
+                        size="sm"
+                        onClick={this.togglecreateModal}
+                      >
+                        <i className="fa fa-plus-square" aria-hidden="true">
+                          {" "}
+                          &nbsp;{" "}
+                        </i>{" "}
+                        New
+                      </Button>
                     </div>
                   </div>
                   &nbsp;&nbsp;&nbsp;
@@ -159,19 +229,19 @@ class MappingHW extends React.Component {
                 </Row>
                 <Row>
                   <Col>
-                    <div >
+                    <div>
                       <Table striped hover bordered responsive size="sm">
                         <thead
                         // style={{ backgroundColor: "#73818f" }}
                         // className="fixed-matlib"
                         >
                           <tr align="center">
-                          {header.map(head => (<th>{head}</th>))}            
+                            {header.map((head) => (
+                              <th>{head}</th>
+                            ))}
                           </tr>
                         </thead>
-                        <tbody>
-                          
-                        </tbody>
+                        <tbody></tbody>
                       </Table>
                     </div>
                   </Col>
@@ -196,17 +266,16 @@ class MappingHW extends React.Component {
             </Card>
           </Col>
         </Row>
-          </div>
-      )
+      </div>
+    );
   }
 }
 
-
 const mapStateToProps = (state) => {
-    return {
-      dataLogin : state.loginData,
-      SidebarMinimize : state.minimizeSidebar
-    }
-  }
-  
-  export default connect(mapStateToProps)(MappingHW);
+  return {
+    dataLogin: state.loginData,
+    SidebarMinimize: state.minimizeSidebar,
+  };
+};
+
+export default connect(mapStateToProps)(MappingHW);
