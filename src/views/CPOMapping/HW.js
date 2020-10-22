c46f2cbd6af7c0aa113ba88b60d91ff753ae1352
mac <mac@macdeMacBook-Pro.local>
2020-10-22 14:51:35 +0700

    f

--- src/views/CPOMapping/HW.js
+++ src/views/CPOMapping/HW.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Loading from "../Component/Loading";
+import { ExcelRenderer } from "react-excel-renderer";
+import {
+  getDatafromAPIMY,
+  postDatatoAPINODE,
+  patchDatatoAPINODE,
+  deleteDataFromAPINODE2,
+  getDatafromAPINODE
+} from "../../helper/asyncFunction";
+import ModalCreateNew from "../Component/ModalCreateNew";
+import Pagination from "react-js-pagination";
+import { saveAs } from "file-saver";
 
 const DefaultNotif = React.lazy(() =>
   import("../../views/DefaultView/DefaultNotif")
 );
 const modul_name = "HW Mapping"
+const header = [
+    'LOOKUP REFERENCE',	'REGION',	'REFERENCE LOC ID',	'NEW LOC ID',	'SITE NAME',	'NEW SITE NAME',	'CONFIG',	'PO#',	'LINE',	'DESCRIPTION',	'QTY',	'NW#',	'CNI DATE',	'MAPPING DATE',	'REMARKS',	'PREMR NO.',	'PROCEED BILLING 100%',	'CELCOM USER',	'PCODE',	'UNIT PRICE',	'TOTAL PRICE',	'DISCOUNTED UNIT PRICE',	'DISCOUNTED PO PRICE',	'SO LINE ITEM DESCRIPTION',	'sitePCode',	'VlookupWBS',	'SO NO.',	'WBS  NO.',	'FOR CHECKING PURPOSE ONLY-RASHIDAH',	'HW COA RECEIVED DATE',	'80% BILLING UPON HW COA',	
+    '80% INVOICING NO.','80% INVOICING DATE',	'NI COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON NI',	'20% INVOICING NO.',	'20% INVOICING DATE',	'SSO COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON SSO',	'20% INVOICING NO.',	'20% INVOICING DATE',	'GR NUMBER',	'HW COA RECEIVED DATE',	'40% BILLING UPON COA HW','40% INVOICING NO.','40% INVOICING DATE','Cancelled',	'NI COA date (vlookup from SERVICES MAPPING)',	'40% BILLING UPON COA NI','40% INVOICING  Number','40% INVOICING DATE','Cancelled',	'SSO COA date (vlookup from SERVICES MAPPING)',	'20% BILLING UPON COA SSO',	'20% INVOICING NO.','20% INVOICING DATE','Cancelled',	'Vlookup: SSO 100% in Service (20% in HW)',	'100%  HW COA ',	'100% BILLING UPON HW COA',	'100% INVOICING NO.',
+    '100% INVOICING DATE','REFERENCE LOC ID',	'PO#',	'REFF',	'SITE LIST',	'REFF 2',	'NI',	'SSO',	'REF NI',
+];
 
 class MappingHW extends React.Component {
   constructor(props) {
@@ -24,12 +41,6 @@ class MappingHW extends React.Component {
     const wb = new Excel.Workbook();
     const ws = wb.addWorksheet();
 
-    let header = [
-        'LOOKUP REFERENCE',	'REGION',	'REFERENCE LOC ID',	'NEW LOC ID',	'SITE NAME',	'NEW SITE NAME',	'CONFIG',	'PO#',	'LINE',	'DESCRIPTION',	'QTY',	'NW#',	'CNI DATE',	'MAPPING DATE',	'REMARKS',	'PREMR NO.',	'PROCEED BILLING 100%',	'CELCOM USER',	'PCODE',	'UNIT PRICE',	'TOTAL PRICE',	'DISCOUNTED UNIT PRICE',	'DISCOUNTED PO PRICE',	'SO LINE ITEM DESCRIPTION',	'sitePCode',	'VlookupWBS',	'SO NO.',	'WBS  NO.',	'FOR CHECKING PURPOSE ONLY-RASHIDAH',	'HW COA RECEIVED DATE',	'80% BILLING UPON HW COA',	
-        '80% INVOICING NO.','80% INVOICING DATE',	'NI COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON NI',	'20% INVOICING NO.',	'20% INVOICING DATE',	'SSO COA DATE (VLOOKUP FROM SERVICES MAPPING)',	'20% BILLING UPON SSO',	'20% INVOICING NO.',	'20% INVOICING DATE',	'GR NUMBER',	'HW COA RECEIVED DATE',	'40% BILLING UPON COA HW','40% INVOICING NO.','40% INVOICING DATE','Cancelled',	'NI COA date (vlookup from SERVICES MAPPING)',	'40% BILLING UPON COA NI','40% INVOICING  Number','40% INVOICING DATE','Cancelled',	'SSO COA date (vlookup from SERVICES MAPPING)',	'20% BILLING UPON COA SSO',	'20% INVOICING NO.','20% INVOICING DATE','Cancelled',	'Vlookup: SSO 100% in Service (20% in HW)',	'100%  HW COA ',	'100% BILLING UPON HW COA',	'100% INVOICING NO.',
-        '100% INVOICING DATE','REFERENCE LOC ID',	'PO#',	'REFF',	'SITE LIST',	'REFF 2',	'NI',	'SSO',	'REF NI',
-    ];
-
     ws.addRow(header);
     for (let i = 1; i < header.length + 1; i++) {
       ws.getCell(numToSSColumn(i) + '1').fill = { type: 'pattern',
@@ -49,10 +60,153 @@ class MappingHW extends React.Component {
           actionMessage={this.state.action_message}
           actionStatus={this.state.action_status}
         />
-        <Row>
-            
+       <Row>
+          <Col xl="12">
+            <Card style={{}}>
+              <CardHeader>
+                <span style={{ marginTop: "8px", position: "absolute" }}>
+                  {" "}
+                  List{" "}{modul_name}
+                </span>
+                <div
+                  className="card-header-actions"
+                  style={{ display: "inline-flex" }}
+                >
+                  <div>
+                    <div>
+                        <Button block color="success" size="sm" onClick={this.togglecreateModal}>
+                          <i className="fa fa-plus-square" aria-hidden="true">
+                            {" "}
+                            &nbsp;{" "}
+                          </i>{" "}
+                          New
+                        </Button>
+       
+                    </div>
+                  </div>
+                  &nbsp;&nbsp;&nbsp;
+                  <div>
+                    <Dropdown
+                      isOpen={this.state.dropdownOpen[1]}
+                      toggle={() => {
+                        this.toggle(1);
+                      }}
+                    >
+                      <DropdownToggle block color="warning" size="sm">
+                        <i className="fa fa-download" aria-hidden="true">
+                          {" "}
+                          &nbsp;{" "}
+                        </i>{" "}
+                        Export
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Uploader Template</DropdownItem>
+                        <DropdownItem onClick={this.exportMatStatus}>
+                          {" "}
+                          Mapping Template
+                        </DropdownItem>
+                        <DropdownItem onClick={this.downloadAll}>
+                          Download All{" "}
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
+                </div>
+              </CardHeader>
+
+              <CardBody>
+                <Row>
+                  {/* <Col>
+                    <div style={{ marginBottom: "10px" }}>
+                      <div
+                        style={{
+                          float: "left",
+                          margin: "5px",
+                          display: "inline-flex",
+                        }}
+                      >
+                        <Input
+                          type="select"
+                          name="select"
+                          id="selectLimit"
+                          onChange={this.handleChangeLimit}
+                        >
+                          <option value={"10"}>10</option>
+                          <option value={"25"}>25</option>
+                          <option value={"50"}>50</option>
+                          <option value={"100"}>100</option>
+                          <option value={"noPg=1"}>All</option>
+                        </Input>
+                      </div>
+                      <div
+                        style={{
+                          float: "right",
+                          margin: "5px",
+                          display: "inline-flex",
+                        }}
+                      >
+                        <input
+                          className="search-box-material"
+                          type="text"
+                          name="filter"
+                          placeholder="Search Material"
+                          onChange={this.handleChangeFilter}
+                          value={this.state.filter_list}
+                        />
+                      </div>
+                    </div>
+                  </Col> */}
+                </Row>
+                <Row>
+                  <Col>
+                    <div >
+                      <Table striped hover bordered responsive size="sm">
+                        <thead
+                        // style={{ backgroundColor: "#73818f" }}
+                        // className="fixed-matlib"
+                        >
+                          <tr align="center">
+                          {header.map(head => (<th>{head}</th>))}            
+                          </tr>
+                        </thead>
+                        <tbody>
+                          
+                        </tbody>
+                      </Table>
+                    </div>
+                  </Col>
+                </Row>
+                <div style={{ margin: "8px 0px" }}>
+                  <small>Showing {this.state.totalData} entries</small>
+                </div>
+                <Row>
+                  <Col>
+                    <Pagination
+                      activePage={this.state.activePage}
+                      itemsCountPerPage={this.state.perPage}
+                      totalItemsCount={this.state.totalData}
+                      pageRangeDisplayed={5}
+                      onChange={this.handlePageChange}
+                      itemClass="page-item"
+                      linkClass="page-link"
+                    />
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
         </Row>
           </div>
       )
   }
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+      dataLogin : state.loginData,
+      SidebarMinimize : state.minimizeSidebar
+    }
+  }
+  
+  export default connect(mapStateToProps)(MappingHW);
