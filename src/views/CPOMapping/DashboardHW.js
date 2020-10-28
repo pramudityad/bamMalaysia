a16cf9f3de8f2910df7d953c51b841d93487eb61
mac <mac@macdeMacBook-Pro.local>
2020-10-28 21:35:36 +0700

    dev#1

---
+++ src/views/CPOMapping/DashboardHW.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Col,
+  FormGroup,
+  Label,
+  Row,
+  Table,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Collapse,
+} from "reactstrap";
+import {
+  getDatafromAPIMY,
+  postDatatoAPINODE,
+  patchDatatoAPINODE,
+  deleteDataFromAPINODE2,
+  getDatafromAPINODE,
+} from "../../helper/asyncFunction";
+import Pagination from "react-js-pagination";
+import { saveAs } from "file-saver";
+import { numToSSColumn } from "../../helper/basicFunction";
+import { connect } from "react-redux";
+
+class ReportHW extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tokenUser: this.props.dataLogin.token,
+      roleUser: this.props.dataLogin.role,
+      dropdownOpen: new Array(3).fill(false),
+      all_data: [],
+      createModal: false,
+      rowsXLS: [],
+      modal_loading: false,
+      prevPage: 0,
+      activePage: 1,
+      totalData: 0,
+      perPage: 10,
+    };
+  }
+
+  componentDidMount() {
+    // console.log("header", header.length);
+    // console.log("model_header", header_model.length);
+    this.getList();
+  }
+
+  getList() {
+    getDatafromAPINODE(
+      "/cpoMapping/getCpo/hw?" +
+        "&lmt=" +
+        this.state.perPage +
+        "&pg=" +
+        this.state.activePage,
+      this.state.tokenUser
+    ).then((res) => {
+      if (res.data !== undefined) {
+        const items = res.data.data;
+        const totalData = res.data.totalResults;
+        this.setState({ all_data: items, totalData: totalData });
+      }
+    });
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
