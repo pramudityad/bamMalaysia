9f84f3ce9d2e397aecb0effb4123755b7755b69f
pramudityad <pramudityad@yahoo.com>
2021-05-05 13:43:38 +0700

    svc global config

--- src/views/CPOMapping/SVC.js
+++ src/views/CPOMapping/SVC.js
@@ -85,265 +85,6 @@ const Checkbox1 = ({
 );
 
 const modul_name = "SVC Mapping";
-const header = [
-  "DEAL NAME",
-  "HAMMER ",
-  "PROJECT DESCRIPTION",
-  "PO NUMBER",
-  "1",
-  "LOOKUP REFERENCE",
-  "REGION",
-  "REFERENCE LOC ID",
-  "NEW LOC ID",
-  "SITE NAME",
-  "NEW SITE NAME",
-  "CONFIG",
-  "PO#",
-  "LINE",
-  "MATERIAL CODE",
-  "LINE ITEM SAP CELCOM",
-  "DESCRIPTION",
-  "QTY",
-  "CNI DATE",
-  "MAPPING DATE",
-  "REMARKS",
-  "GR NO",
-  "PROCEED BILLING 100%",
-  "CELCOM USER",
-  "PCODE",
-  "UNIT PRICE",
-  "TOTAL PRICE",
-  "COMMODITY",
-  "DISCOUNTED UNIT PRICE",
-  "DISCOUNTED PO PRICE",
-  "NET UNIT PRICE",
-  "INVOICE TOTAL",
-  "HAMMER 1 HD TOTAL",
-  "SO LINE ITEM DESCRIPTION",
-  "sitePCode",
-  "VlookupWBS",
-  "SO NO.",
-  "WBS  NO.",
-  "100% BILLING",
-  "ATP COA DATE RECEIVED",
-  "80% BILLING UPON ATP",
-  "80% INVOICING NO.",
-  "80% INVOICING DATE",
-  "Cancelled",
-  "COA NI DATE RECEIVED",
-  "20% BILLING UPON NI",
-  "20% INVOICING NO.",
-  "20% INVOICING DATE",
-  "Cancelled",
-  "COA SSO RCVD DATE",
-  "80% BILLING UPON SSO",
-  "80% INVOICING NO.",
-  "80% INVOICING DATE",
-  "Cancelled",
-  "COA PSP RCVD DATE 20%",
-  "20% BILLING UPON PSP",
-  "20% INVOICING NO.",
-  "20% INVOICING DATE",
-  "Cancelled",
-  "COA NI RCVD DATE",
-  "40% BILLING UPON NI",
-  "40% INVOICING NO.",
-  "40% INVOICING DATE",
-  "Cancelled",
-  "COSSO RCVD DATE",
-  "60% BILLING UPON SSO",
-  "60% INVOICING NO.",
-  "60% INVOICING DATE",
-  "Cancelled",
-  "COA SSO RCVD DATE 100%",
-  "100% BILLING UPON SSO",
-  "100% INVOICING NO.",
-  "100% INVOICING DATE",
-  "Cancelled",
-  "COA NI RCVD DATE 100%",
-  "100% BILLING UPON NI",
-  "100% INVOICING NO.",
-  "100% INVOICING DATE",
-  "Cancelled",
-  "SES NO.",
-  "SES STATUS",
-  "LINK",
-  "NI COA SUBMISSION STATUS",
-];
-
-const header_model = [
-  "Deal_Name",
-  "Hammer",
-  "Project_Description",
-  "Po_Number",
-  "Data_1",
-  "Lookup_Reference",
-  "Region",
-  "Reference_Loc_Id",
-  "New_Loc_Id",
-  "Site_Name",
-  "New_Site_Name",
-  "Config",
-  "Po",
-  "Line",
-  "Material_Code",
-  "Description",
-  "Line_Item_Sap",
-  "Qty",
-  "CNI_Date",
-  "Mapping_Date",
-  "Remarks",
-  "Gr_No",
-  "Proceed_Billing_100",
-  "Celcom_User",
-  "Pcode",
-  "Unit_Price",
-  "Total_Price",
-  "Commodity",
-  "Discounted_Unit_Price",
-  "Discounted_Po_Price",
-  "Net_Unit_Price",
-  "Invoice_Total",
-  "Hammer_1_Hd_Total",
-  "So_Line_Item_Description",
-  "Sitepcode",
-  "VlookupWbs",
-  "So_No",
-  "Wbs_No",
-  "Billing_100",
-  "Atp_Coa_Received_Date_80",
-  "Billing_Upon_Atp_Coa_80",
-  "Invoicing_No_Atp_Coa_80",
-  "Invoicing_Date_Atp_Coa_80",
-  "Cancelled_Atp_Coa_80",
-  "Ni_Coa_Date_20",
-  "Billing_Upon_Ni_20",
-  "Invoicing_No_Ni_20",
-  "Invoicing_Date_Ni_20",
-  "Cancelled_Invoicing_Ni_20",
-  "Sso_Coa_Date_80",
-  "Billing_Upon_Sso_80",
-  "Invoicing_No_Sso_80",
-  "Invoicing_Date_Sso_80",
-  "Cancelled_Sso_Coa_Date_80",
-  "Coa_Psp_Received_Date_20",
-  "Billing_Upon_Coa_Psp_20",
-  "Invoicing_No_Coa_Psp_20",
-  "Invoicing_Date_Coa_Psp_20",
-  "Cancelled_Coa_Psp_Received_Date_20",
-  "Coa_Ni_Received_Date_40",
-  "Billing_Upon_Coa_Ni_40",
-  "Invoicing_No_Coa_Ni_40",
-  "Invoicing_Date_Coa_Ni_40",
-  "Cancelled_Coa_Ni_Received_Date_40",
-  "Cosso_Received_Date_60",
-  "Billing_Upon_Cosso_60",
-  "Invoicing_No_Cosso_60",
-  "Invoicing_Date_Cosso_60",
-  "Cancelled_Cosso_Received_Date_60",
-  "Coa_Sso_Received_Date_100",
-  "Billing_Upon_Sso_Coa_100",
-  "Invoicing_No_Sso_Coa_100",
-  "Invoicing_Date_Sso_Coa_100",
-  "Cancelled_Coa_Sso_Received_Date_100",
-  "Coa_Ni_Date_100",
-  "Billing_Upon_Coa_Ni_100",
-  "Invoicing_No_Coa_Ni_100",
-  "Invoicing_Date_Coa_Ni_100",
-  "Cancelled_Coa_Ni_Date_100",
-  "Ses_No",
-  "Ses_Status",
-  "Link",
-  "Ni_Coa_Submission_Status",
-];
-
-const header_materialmapping = [
-  "Deal_Name",
-  "Hammer",
-  "Project_Description",
-  "Po_Number",
-  "Data_1",
-  "Lookup_Reference",
-  "Region",
-  "Reference_Loc_Id",
-  "New_Loc_Id",
-  "Site_Name",
-  "New_Site_Name",
-  "Config",
-  "Po",
-  "Line",
-  "Description",
-  "Qty",
-  "CNI_Date",
-  "Mapping_Date",
-  "Remarks",
-  "Gr_No",
-
-  "Premr_No",
-  "Proceed_Billing_100",
-  "Celcom_User",
-  "Pcode",
-  "Unit_Price",
-  "Total_Price",
-  "Commodity",
-  "Discounted_Unit_Price",
-  "Discounted_Po_Price",
-];
-
-const header_pfm = [
-  "So_Line_Item_Description",
-  "Sitepcode",
-  "VlookupWbs",
-  "So_No",
-  "Wbs_No",
-  // "Atp_Coa_Received_Date_80",
-  "Billing_Upon_Atp_Coa_80",
-  "Invoicing_No_Atp_Coa_80",
-  "Invoicing_Date_Atp_Coa_80",
-  "Cancelled_Atp_Coa_80",
-  // "Ni_Coa_Date_20",
-  "Billing_Upon_Ni_20",
-  "Invoicing_No_Ni_20",
-  "Invoicing_Date_Ni_20",
-  "Cancelled_Invoicing_Ni_20",
-  // "Sso_Coa_Date_80",
-  "Billing_Upon_Sso_80",
-  "Invoicing_No_Sso_80",
-  "Invoicing_Date_Sso_80",
-  "Cancelled_Sso_Coa_Date_80",
-  // "Coa_Psp_Received_Date_20",
-  "Billing_Upon_Coa_Psp_20",
-  "Invoicing_No_Coa_Psp_20",
-  "Invoicing_Date_Coa_Psp_20",
-  "Cancelled_Coa_Psp_Received_Date_20",
-  // "Coa_Sso_Received_Date_100",
-  "Billing_Upon_Sso_Coa_100",
-  "Invoicing_No_Sso_Coa_100",
-  "Invoicing_Date_Sso_Coa_100",
-  "Cancelled_Coa_Sso_Received_Date_100",
-  "Coa_Ni_Date_100",
-  "Billing_Upon_Coa_Ni_100",
-  "Invoicing_No_Coa_Ni_100",
-  "Invoicing_Date_Coa_Ni_100",
-  "Cancelled_Coa_Ni_Date_100",
-];
-
-const header_admin = [
-  "Proceed_Billing_100",
-  "Billing_100",
-  "Atp_Coa_Received_Date_80",
-  "Ni_Coa_Date_20",
-  "Sso_Coa_Date_80",
-  "Coa_Psp_Received_Date_20",
-  "Coa_Ni_Received_Date_40",
-  "Cosso_Received_Date_60",
-  "Coa_Sso_Received_Date_100",
-  "Coa_Ni_Date_100",
-  "Ses_No",
-  "Ses_Status",
-  "Link",
-  "Ni_Coa_Submission_Status",
-];
 
 class MappingSVC extends React.PureComponent {
   constructor(props) {
@@ -386,11 +127,6 @@ class MappingSVC extends React.PureComponent {
   }
 
   componentDidMount() {
-    // console.log("header", header.length);
-    // console.log("model_header", header_model.length);
-    // this.getList();
-    // this.getHeader();
-    // this.getListAll();
     this.getMaster();
   }
 
@@ -944,10 +680,14 @@ class MappingSVC extends React.PureComponent {
         : this.state.roleUser.includes("BAM-PFM") === true
         ? 2
         : 3;
-    const header_create_not_req = [header_materialmapping];
+    const header_create_not_req = [
+      global.config.cpo_mapping.svc.header_materialmapping,
+    ];
     const body_create_not_req = this.state.dataChecked_container.map((data) =>
       Object.keys(data)
-        .filter((key) => header_materialmapping.includes(key))
+        .filter((key) =>
+          global.config.cpo_mapping.svc.header_materialmapping.includes(key)
+        )
         .reduce((obj, key) => {
           obj[key] = data[key];
           return obj;
@@ -1124,7 +864,11 @@ class MappingSVC extends React.PureComponent {
 
   loopSearchBar = () => {
     let searchBar = [];
-    for (let i = 0; i < header_model.length; i++) {
+    for (
+      let i = 0;
+      i < global.config.cpo_mapping.svc.header_model.length;
+      i++
+    ) {
       searchBar.push(
         <td>
           {/* {i !== 0 && i !== 3 && i !== 5 && i !== 7 && i !== 9 && i !== 10 ? (
@@ -1142,8 +886,12 @@ class MappingSVC extends React.PureComponent {
                 type="text"
                 placeholder="Search"
                 onChange={this.handleFilterList}
-                value={this.state.filter_list[header_model[i]]}
-                name={header_model[i]}
+                value={
+                  this.state.filter_list[
+                    global.config.cpo_mapping.svc.header_model[i]
+                  ]
+                }
+                name={global.config.cpo_mapping.svc.header_model[i]}
                 size="sm"
               />
             </InputGroup>
@@ -1464,9 +1212,11 @@ class MappingSVC extends React.PureComponent {
                             ) : (
                               ""
                             )}
-                            {header.map((head) => (
-                              <th>{head}</th>
-                            ))}
+                            {global.config.cpo_mapping.svc.header.map(
+                              (head) => (
+                                <th>{head}</th>
+                              )
+                            )}
                           </tr>
                           {this.state.tabs_submenu[0] === true ? (
                             <>
@@ -1488,9 +1238,11 @@ class MappingSVC extends React.PureComponent {
                           ) : (
                             <>
                               <tr align="center">
-                                {header_model.map((head) => (
-                                  <th>{this.countheaderNaN(head)}</th>
-                                ))}
+                                {global.config.cpo_mapping.svc.header_model.map(
+                                  (head) => (
+                                    <th>{this.countheaderNaN(head)}</th>
+                                  )
+                                )}
                               </tr>
                             </>
                           )}
