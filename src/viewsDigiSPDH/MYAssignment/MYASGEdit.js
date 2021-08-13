352c8b0f7375579f1b5fcbbce53c49e3290199a6
farizmursyidan <farizmursyidan@gmail.com>
2021-08-13 13:57:40 +0700

    add log and validation wp id

--- src/viewsDigiSPDH/MYAssignment/MYASGEdit.js
+++ src/viewsDigiSPDH/MYAssignment/MYASGEdit.js
@@ -355,6 +355,12 @@ class MYASGEdit extends Component {
       dataLMR[parseInt(idx)]["m_id_wp"] = findWPID.data.result.m_id;
     } else {
       dataLMR[parseInt(idx)]["m_id_wp"] = "";
+      let data_log = {
+        lmr_id: "LMR has not been created, cannot get WP ID",
+        wp_id: value,
+        response_act: JSON.stringify(findWPID)
+      }
+      await postDatatoAPINODE("/aspassignment/submitLogErisite", { data: data_log }, this.state.tokenUser);
     }
 
     this.setState({ creation_lmr_child_form: dataLMR }, () =>
@@ -771,6 +777,22 @@ class MYASGEdit extends Component {
         // dataChildForm[i].duplicate = 'no';
         dataChildForm[i].blank_material = 'no';
       }
+      this.toggleLoading();
+    } else if (dataChildForm.some(e => e.m_id_wp === '')) {
+      const getAlert = () => (
+        <SweetAlert
+          danger
+          title="Error!"
+          onConfirm={() => this.hideAlert()}
+        >
+          There's a problem when fetching the WP ID data, please reselect the WP ID!
+        </SweetAlert>
+      );
+
+      this.setState({
+        sweet_alert: getAlert()
+      });
+
       this.toggleLoading();
     } else {
       console.log("dataLMR", dataLMR);
